21ee74aa0cf50160422f70ca03123a92257ccca1
CaiquePrado <caiqueprado76@gmail.com>
2023-05-17 14:19:49 -0300

    feat: know-techs components

---
+++ app/components/pages/home/highlighted-pojects/index.tsx
@@ -0,0 +1,9 @@
+import { SectionTitle } from "../../../section-title";
+
+export const HighLightedProject = () => {
+  return (
+    <section className="container py-16">
+      <SectionTitle subtitle="Destaques" title="Pojetos em destaque" />
+    </section>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import { HeroSection } from "./components/pages/home/hero-section";
+import { HighLightedProject } from "./components/pages/home/highlighted-pojects";
+import { KnowTechs } from "./components/pages/home/know-techs";
 
 export default async function Home() {
   return (
     <>
       <HeroSection />
+      <KnowTechs />
+      <HighLightedProject />
     </>
   );
 }
