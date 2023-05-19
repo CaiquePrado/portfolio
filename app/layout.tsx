d9f21c34cf599ff39d11d107ef44ce075ea6df95
CaiquePrado <caiqueprado76@gmail.com>
2023-05-19 15:17:42 -0300

    feat: scroll on top

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { IBM_Plex_Mono, Inter } from "next/font/google";
 import { ReactNode } from "react";
+import { BackToTop } from "./components/back-to-top";
 import { ContactForm } from "./components/contact-form";
 import { Footer } from "./components/footer";
 import { Header } from "./components/header";
@@ -20,6 +21,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR" className={`${inter.variable} ${plexMono.variable}`}>
       <body>
+        <BackToTop />
         <Header />
         {children}
         <ContactForm />
